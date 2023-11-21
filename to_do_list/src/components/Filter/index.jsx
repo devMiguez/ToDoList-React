8308a6d7b3f3b86f99619b6b0b4112dd68d3d401
joao <joaovmiguez@gmail.com>
2023-11-21 15:03:57 -0300

    Creating a mobile design of 'Filter task'

--- to_do_list/src/components/Filter/index.jsx
+++ to_do_list/src/components/Filter/index.jsx
@@ -3,9 +3,19 @@ import './style.css'
 export default function Filter(props){
 
     return(
-        <div className="Filter_itens">
-            
+        <div className="filter_itens">
+            <h1 className="sub_title">Filter: </h1>
 
+            <div className="content_filter">
+
+                <label for="statusTask">Status: </label>
+                <select name="status" id="statusTask">
+                    <option value="all">All tasks</option>
+                    <option value="Complete">Complete</option>
+                    <option value="todo">ToDo</option>
+                </select>
+
+            </div>
             
         </div>
     );
--- to_do_list/src/components/Search/index.jsx
+++ to_do_list/src/components/Search/index.jsx
@@ -5,7 +5,7 @@ export default function Search(props){
     return(
         <>
             <div className="search_item">
-                <h1 className='sub_title'>Search task: </h1>
+                <h1 className='sub_title'>Task search: </h1>
                 <input type="text" placeholder="Search your task..."/>
             </div>
         </>
