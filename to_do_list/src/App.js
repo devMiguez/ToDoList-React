42ebba175c2766bb432d3f34093181ff321245ff
joao <joaovmiguez@gmail.com>
2023-11-21 13:51:18 -0300

    Doing media-queries using mobile first method

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main id="container_principal">
+        
+        <section className='content_main'>
+
+            <div className='title'> 
+                <h1>todo<span>-list</span></h1>
+            </div>
+
+        </section>
+
+    </main>
   );
 }
 
