28230511354342e5aca097395586be9643fe4d80
joao <joaovmiguez@gmail.com>
2023-11-21 16:14:25 -0300

    Creating a mobile design of 'Create task' and mobile design done

--- to_do_list/src/components/BoxList/index.jsx
+++ to_do_list/src/components/BoxList/index.jsx
@@ -2,7 +2,7 @@ import './style.css'
 
 export default function BoxList(props) {
     return (
-        <div className="box_tasks">
+        <div className="tasks_box">
             <div className="name-task">
                 <h1>Nome tarefa</h1>
                 <p>(categoria)</p>
--- to_do_list/src/components/CreateTask/index.jsx
+++ to_do_list/src/components/CreateTask/index.jsx
@@ -3,8 +3,26 @@ import './style.css'
 export default function CreateTask(props){
 
     return(
-        <div>
+        <div className="create">
+            <h1 className="sub_title">Create task: </h1>
 
+            <input type="text" placeholder="Enter the task..." />
+
+            <div className="choose_category">
+
+                <label for="categoryTask">Choose a category: </label>
+                <select name="category" id="categoryTask">
+                    <option value="choose">Categories:</option>
+                    <option value="study">Study</option>
+                    <option value="work">Work</option>
+                    <option value="personal">Personal</option>
+                </select>
+
+            </div>
+
+            <div className="btn">
+                <button>Create</button>
+            </div>
         </div>
     );
 
